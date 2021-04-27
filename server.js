9e4b4a9c1d4e6043b04b69a81355200626a95d3f
Lawrence Yuen <lwrnceyuen@gmail.com>
2021-04-27 01:56:55 -0700

    connect to mysql database

---
+++ server.js
@@ -0,0 +1,37 @@
+const express = require('express');
+const mysql = require('mysql2');
+
+const PORT = process.env.PORT || 3001;
+const app = express();
+
+// Express middleware
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+// Connect to database
+const db = mysql.createConnection(
+  {
+    host: 'localhost',
+    // Your MySQL username,
+    user: 'root',
+    // Your MySQL password
+    password: 'MomsBday931962!',
+    database: 'election'
+  },
+  console.log('Connected to the election database.')
+);
+
+app.get('/', (req, res) => {
+  res.json({
+    message: 'Hello World'
+  });
+});
+
+// Default response for any other request (Not Found)
+app.use((req, res) => {
+  res.status(404).end();
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
